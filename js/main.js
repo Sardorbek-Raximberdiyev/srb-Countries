e398437831500246084835f038167bb6071e900d
Sardorbek-Raximberdiyev <sardorraximberdiyev9@gmail.com>
2022-04-12 18:07:39 +0500

    Correction js styles

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
-const siteheaderbtn = document.querySelector('site-header-btn');
+document.addEventListener("DOMContentLoaded", function () {
+  const theme = localStorage.getItem("theme", "dark");
+  const darkModeToggle = document.getElementById("chk");
 
-siteheaderbtn.addEventListener('click', function () {
-  document.body.classList.add
-})
+  if (theme === "dark") {
+    document.documentElement.classList.add("dark-mode");
+  } else {
+    document.documentElement.classList.remove("dark-mode");
+  }
+
+  darkModeToggle.addEventListener("click", function () {
+    document.documentElement.classList.toggle("dark-mode");
+
+    if (document.documentElement.classList.contains("dark-mode")) {
+      localStorage.setItem("theme", "dark");
+    } else {
+      localStorage.setItem("theme", "light");
+    }
+  });
+});
